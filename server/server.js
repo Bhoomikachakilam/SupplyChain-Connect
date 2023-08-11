6ebbd5bf54d17c22baf5e1c517b74d094f43305b
Bhoomikachakilam <122263678+Bhoomikachakilam@users.noreply.github.com>
2023-08-11 13:12:47 +0530

    update

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(orderrouter);
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://supply-chain-connect.vercel.app/",
+    origin: "https://supplychainconnect.onrender.com",
     // origin: "http://localhost:3000",
     methods: ["GET", "POST"],
   },
